023f1a7406006f689c6a8fa7cb994e0487bb7063
Dheeraj Anumula <dheeraj_anumula@epam.com>
2022-08-02 23:38:33 +0530

    Integrated import products service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://8ui2filzzh.execute-api.ap-south-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://os1u2n4md2.execute-api.ap-south-1.amazonaws.com/dev",
+  import: "https://k7ejrwnx0b.execute-api.ap-south-1.amazonaws.com/dev",
+  bff: "https://8ui2filzzh.execute-api.ap-south-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
